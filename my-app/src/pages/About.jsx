0c5948f0cb8d8c24d8b46770779a02894565cca5
aleksandrstarshynov <aleksandrstarshynov@gmail.com>
2025-04-29 10:28:26 +0200

    header with links added

---
+++ my-app/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function About() {
+  return (
+    <>
+      <main>
+        <h1>About</h1>
+      </main>
+    </>
+  );
+}
+
+export default About;
