dd0e6b96e360a63ba60b29234c32fcc23fd16182
aleksandrstarshynov <aleksandrstarshynov@gmail.com>
2025-05-08 18:51:07 +0200

    server added

---
+++ server/users.js
@@ -0,0 +1,18 @@
+import newDatabase from './database.js'
+// import { hash, compare } from 'bcrypt';
+
+// Change this boolean to true if you wish to keep your
+// users between restart of your application
+const isPersistent = true
+const database = newDatabase({isPersistent});
+
+export const getSessionId = (req) => {
+  const authorizationHeader = req.headers['authorization'];
+  if(!authorizationHeader) {
+    return null;
+  }
+  const sessionId = authorizationHeader.replace('Bearer ', '');
+  return sessionId.trim();
+};
+
+export { database };
