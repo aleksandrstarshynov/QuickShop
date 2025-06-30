f611f79f0716a0c6daf74377e6f06b708ecbaba6
aleksandrstarshynov <aleksandrstarshynov@gmail.com>
2025-06-30 14:28:05 +0200

    cors fixed

--- my-app/src/components/CheckoutForm.js
+++ my-app/src/components/CheckoutForm.js
@@ -32,7 +32,7 @@ export default function CheckoutForm() {
     if (!cart?.length) return;
     (async () => {
       try {
-        const resp = await fetch('http://localhost:4000/create-payment-intent', {  //TODO
+        const resp = await fetch(`${API_BASE}/create-payment-intent`, {  
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ items: cart }),
